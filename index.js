5f6998af4bac90df8ba0c22809b4f6cc48ca1ed0
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-09-23 04:35:09 +0000

    アクセス時刻を返す変更

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   const now = new Date().getTime();
-  res.setHeader('Set-Cookie', `last_access=${now};`)
-  res.end('hi');
+  res.setHeader('Set-Cookie', `last_access=${now};`);
+  const cookie = req.headers.cookie;
+  const last_access_time = cookie
+    ? parseInt(cookie.split('last_access=')[1])
+    : now;
+  res.end(new Date(last_access_time).toString());
 });
 const port = 8000;
 server.listen(port, () => {
